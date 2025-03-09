fa49d92a2a68c8fa5d79abcbaa96657bc6e857e1
Shl Xue <xueshuiliang@gmail.com>
2025-03-09 22:43:47 +0800

    Add spring boot module

---
+++ spring-boot/spring-boot-hop-autoconfigure/src/main/java/org/apache/hop/spring/autoconfigure/HopAutoConfiguration.java
@@ -0,0 +1,7 @@
+package org.apache.hop.spring.autoconfigure;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties(HopProperties.class)
+public class HopAutoConfiguration {
+}
---
+++ spring-boot/spring-boot-hop-autoconfigure/src/main/java/org/apache/hop/spring/autoconfigure/HopProperties.java
@@ -0,0 +1,7 @@
+package org.apache.hop.spring.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "hop")
+public class HopProperties {
+}
